1edeec65e9e792ff71cf53402dc27d8bd4863ba7
frans <frans.eriksson@hotmail.com>
2021-01-03 22:30:51 +0100

    minor changes to add list page

--- lib/pages/AddListPage.dart
+++ lib/pages/AddListPage.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
 
-class AddListPage extends StatelessWidget {
+class AddListPage extends StatefulWidget {
+  @override
+  _AddListPageState createState() => _AddListPageState();
+}
+
+class _AddListPageState extends State<AddListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepOrange,
-      body: Column(
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            _form(), _list()],
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        child: Icon(Icons.add, color: Colors.black,),
+        onPressed: () {},
       ),
     );
   }
 }
+
+_form() => Text('form goes here');
+
+_list() => Text('list of contacts goes here');
